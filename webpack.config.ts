f9f2df1b830acf0303ac21f6b9263da2de68f81d
Mauricio Rivera <mauriver21@gmail.com>
2022-04-26 22:26:22 -0500

    Minimal project setup done from scratch.

---
+++ webpack.config.ts
@@ -0,0 +1,46 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import 'webpack-dev-server';
+
+const config: Configuration = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'index.html' })],
+  devServer: {
+    open: true,
+  },
+};
+
+export default config;
